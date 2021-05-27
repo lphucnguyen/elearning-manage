938379489ec76605ab0c7d895454a997b4f1a13d
dangtdn <nguyendanglop93@gmail.com>
2021-05-27 17:21:52 +0700

    'update_Loading'

---
+++ src/configs/settings.js
@@ -0,0 +1,2 @@
+export const domain = 'http://elearning0706.cybersoft.edu.vn';
+export const token = localStorage.getItem('accessToken')
---
+++ src/services/UserServices.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import axios from "axios";
+import {domain, token} from '../configs/settings';
+
+function UserServices() {
+
+    dangKy = (data) => {
+        return axios({
+          url: `${domain}/api/QuanLyNguoiDung/DangKy`,
+          method: "POST",
+          data,
+        });
+    };
+    dangNhap = ({ taiKhoan, matKhau }) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/DangNhap`,
+            method: "POST",
+            data: { taiKhoan, matKhau },
+        });
+    };
+    thongTinTaiKhoan = ({taiKhoan,matKhau}) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/ThongTinTaiKhoan`,
+            method: "POST",
+            data: {taiKhoan,matKhau},
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+    
+    layDanhSachNguoiDung = (group) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=${group}`,
+            method: "GET",
+        });
+    };
+
+    layDanhSachNguoiDung_PhanTrang = (group,page) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/LayDanhSachNguoiDung_PhanTrang?MaNhom=${group}&page=${page}&pageSize=20`,
+            method: "GET",
+        });
+    };
+
+    themNguoiDung = (values) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/ThemNguoiDung`,
+            method: "POST",
+            data: values,
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+    capNhatThongTinNguoiDung = (value) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung`,
+            method: "PUT",
+            data: value,
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+    xoaNguoiDung = (id) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${id}`,
+            method: "DELETE",
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+
+    layDanhSachKhoaHocChuaGhiDanh = ({taiKhoan}) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/LayDanhSachKhoaHocChuaGhiDanh`,
+            method: "POST",
+            data: {taiKhoan},
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+
+    layDanhSachKhoaHocChoXetDuyet = ({taiKhoan}) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/LayDanhSachKhoaHocChoXetDuyet`,
+            method: "POST",
+            data: {taiKhoan},
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+
+    layDanhSachKhoaHocDaXetDuyet = ({taiKhoan}) => {
+        return axios({
+            url: `${domain}/api/QuanLyNguoiDung/LayDanhSachKhoaHocDaXetDuyet`,
+            method: "POST",
+            data: {taiKhoan},
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    };
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserServices;
