52d49820f3716df810871a153bee8cffeadb0194
Nguyen <nhoxzznhox15@gmail.com>
2021-05-29 16:57:16 +0700

    Add header

---
+++ src/Components/layouts/Carousel/Carousel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Carousel.scss"
+
+function Carousel() {
+    return (
+        <div className="container-fluid">
+            <div className="carousel">
+                <div className="container">
+                    
+                </div>                
+            </div>
+        </div>
+    )
+}
+
+export default Carousel;
