f3b0ba6a76863e7540534a3788d467319d930e7d
dangtdn <nguyendanglop93@gmail.com>
2021-06-06 23:59:19 +0700

    'update_userManage-finished'

--- src/Components/layouts/admin/UserManage/UserManage.js
+++ src/Components/layouts/admin/UserManage/UserManage.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import HeaderAdmin from '../../../../common/Admin/HeaderAdmin/HeaderAdmin';
+import SliderbarAdmin from '../../../../common/Admin/SliderbarAdmin/SliderbarAdmin';
+import MainUserManage from '../MainUserManage/MainUserManage';
 import './UserManage.scss'
 
 function UserManage() {
+
     return (
-        <div>
-            
+        <div className="wrapper collapse-slide">
+            <HeaderAdmin/>
+            <SliderbarAdmin/>
+            <MainUserManage/>
         </div>
+
     )
 }
 
